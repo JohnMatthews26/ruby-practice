a8583fd1259ad6d246fcd90756af63b7a0a8845c
John Matthews <johnmatthews2634@gmail.com>
2017-08-22 11:31:13 -0700

    algo proj 6

--- algorithms_project_6/john_matthews/lib/install_order.rb
+++ algorithms_project_6/john_matthews/lib/install_order.rb
@@ -10,5 +10,30 @@
 
 
 def install_order(arr)
+  new_range = (arr.min.min..arr.max.max).to_a
+  result_arr = []
+  tup_hash = {}
+  arr.each do |subarr|
+    if tup_hash[subarr[0]]
+      tup_hash[subarr[0]] = tup_hash[subarr[0]].push(subarr[1])
+    else
+      tup_hash[subarr[0]] = [subarr[1]]
+    end
+    new_range.delete(subarr[0])
+  end
+  until tup_hash.keys.empty?
+    tup_hash.keys.each do |tup|
+      if tup_hash[tup].any? { |el| tup_hash.keys.include?(el) }
 
+      else
+        result_arr.push(tup)
+        tup_hash.delete(tup)
+      end
+    end
+  end
+
+
+  new_range.concat(result_arr)
+  p new_range
+  return new_range
 end
