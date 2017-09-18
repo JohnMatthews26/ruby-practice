9e5c5b3e862fa2fd470e18f47090933896e39e9d
John Matthews <johnmatthews2634@gmail.com>
2017-09-18 16:52:56 -0700

    take console input and output all animals with that letter

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -1,20 +1,31 @@
-// const fs = require('fs');
-//
-// fs.readFile('./animals.txt', 'utf-8',(err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(data);
-// });
-
-
 const fs = require('fs');
 
-fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+
+var array = fs.readFileSync('./animals.txt').toString().split("\n");
+
+let valid = [];
+
+array.forEach(function(el){
+  console.log(el[0], process.argv[2]);
+  if (el[0] === process.argv[2]) {
+    valid.push(el);
+  }
+});
+
+fs.writeFile(`./${process.argv[2]}_animals.txt`, `${valid}`, err => {
   if (err) {
     console.log(err);
   } else {
     console.log("file successfully written!");
   }
-})
+});
+//
+// fs.writeFile('./{process.argv[2]}_animals.txt', 'I will be written to example.txt', err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file successfully written!");
+//   }
+// });
+//
+// console.log(process.argv);
