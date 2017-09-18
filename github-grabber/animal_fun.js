1961d7701d67c341d50c0d4cb6307400e1e5ec76
John Matthews <johnmatthews2634@gmail.com>
2017-09-18 16:29:13 -0700

    fs-readfile

---
+++ github-grabber/animal_fun.js
@@ -0,0 +1,20 @@
+// const fs = require('fs');
+//
+// fs.readFile('./animals.txt', 'utf-8',(err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+const fs = require('fs');
+
+fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("file successfully written!");
+  }
+})
