98d9ecba28b1f6f0d34e2f8bcc88f7b892361b72
John Matthews <johnmatthews2634@gmail.com>
2017-09-20 10:25:48 -0700

    modify to accept lowercase input

--- github-grabber/animal_fun.js
+++ github-grabber/animal_fun.js
@@ -5,27 +5,19 @@ var array = fs.readFileSync('./animals.txt').toString().split("\n");
 
 let valid = [];
 
+let userInput = process.argv[2].toUpperCase();
+
 array.forEach(function(el){
-  console.log(el[0], process.argv[2]);
-  if (el[0] === process.argv[2]) {
+
+  if (el[0] === userInput) {
     valid.push(el);
   }
 });
 
-fs.writeFile(`./${process.argv[2]}_animals.txt`, `${valid}`, err => {
+fs.writeFile(`./${userInput}_animals.txt`, `${valid}`, err => {
   if (err) {
     console.log(err);
   } else {
     console.log("file successfully written!");
   }
 });
-//
-// fs.writeFile('./{process.argv[2]}_animals.txt', 'I will be written to example.txt', err => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("file successfully written!");
-//   }
-// });
-//
-// console.log(process.argv);
