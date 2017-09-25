63fdb9950bc38fd95936b1fbe56fcc2305cad05e
John Matthews <johnmatthews2634@gmail.com>
2017-09-25 15:43:47 -0700

    add app.js

---
+++ chat-app-skeleton/public/javascripts/index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = require('socket.io-client')();
+  const ChatUI = require('./chatUI');
+  const myChat = new ChatUI(socket);
+  console.log(socket);
+
+  
+});
