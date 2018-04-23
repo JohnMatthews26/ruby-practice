c1f0f6652bee443fa17cb61856860903ac5c428a
John Matthews <johnmatthews2634@gmail.com>
2018-04-23 13:23:16 -0700

    add bench bnb skelskeleton

---
+++ test.js
@@ -0,0 +1,7 @@
+<div id="doubleHolder">
+  <button onClick={returnButtons()}></button>
+</div>
+
+function returnButtons() {
+return <button></button><button></button>;
+}
---
+++ test.jsx
@@ -0,0 +1,3 @@
+<div id="doubleHolder">
+
+</div>
